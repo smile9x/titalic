54c2607e4a3622353ed37ad1f39cbe713b225513
Cuong Ngo <smile9x@gmail.com>
2018-02-13 00:05:19 +0700

    creating categories

--- script.py
+++ script.py
@@ -123,3 +123,72 @@ for dataset in data:
     
     
 train_df["Age"].isnull().sum()
+
+train_df['Embarked'].describe()
+
+
+common_value = 'S'
+data = [train_df, test_df]
+
+for dataset in data:
+    dataset['Embarked'] = dataset['Embarked'].fillna(common_value)
+  
+    
+train_df.info()
+
+
+#Fare
+data = [train_df, test_df]
+for dataset in data:
+    dataset['Fare'] = dataset['Fare'].fillna(0)
+    dataset['Fare'] = dataset['Fare'].astype(int)
+    
+#Name
+data = [train_df, test_df]
+titles = {"Mr":1, "Miss":2, "Mrs":3, "Master":4, "Rare":5}
+for dataset in data:
+    #extract titles
+    dataset['Title'] = dataset.Name.str.extract('([A-Za-z+])\.',expand=False)
+    #replace titles with a more common title or as Rare
+    dataset['Title'] = dataset['Title'].replace(['Lady', 'Countess', 'Capt', 'Col', 'Don', 'Dr', 'Major', 'Rev', 'Sir', 'Jonkheer', 'Dona'], 'Rare')
+    dataset['Title'] = dataset['Title'].replace('Ms', 'Miss')
+    dataset['Title'] = dataset['Title'].replace('Mme', 'Mrs')
+    
+    # convert titles into numbers
+    dataset['Title'] = dataset['Title'].map(titles)
+    # filling NaN with 0, to get safe
+    dataset['Title'] = dataset['Title'].fillna(0)
+    
+train_df = train_df.drop(['Name'], axis=1)
+test_df = test_df.drop(['Name'], axis=1)
+
+#Sex
+# convert sex feature into numeric
+genders = {"male": 0, "female": 1}
+data = [train_df, test_df]
+
+#Ticket:
+train_df['Ticket'].describe()
+
+train_df = train_df.drop(['Ticket'], axis=1)
+test_df = test_df.drop(['Ticket'], axis=1)
+
+#Embarked
+ports = {"S":0, "C":1, "Q": 2}
+data = [train_df, test_df]
+for dataset in data:
+    dataset['Embarked'] = dataset['Embarked'].map(ports)
+    
+#Creating Categories
+#Age
+data = [train_df, test_df]
+for dataset in data:
+    dataset['Age'] = dataset['Age'].astype(int)
+    dataset.loc[ dataset['Age'] <= 11, 'Age'] = 0
+    dataset.loc[(dataset['Age'] > 11) & (dataset['Age'] <= 18), 'Age'] = 1
+    dataset.loc[(dataset['Age'] > 18) & (dataset['Age'] <= 22), 'Age'] = 2
+    dataset.loc[(dataset['Age'] > 22) & (dataset['Age'] <= 27), 'Age'] = 3
+    dataset.loc[(dataset['Age'] > 27) & (dataset['Age'] <= 33), 'Age'] = 4
+    dataset.loc[(dataset['Age'] > 33) & (dataset['Age'] <= 40), 'Age'] = 5
+    dataset.loc[(dataset['Age'] > 40) & (dataset['Age'] <= 66), 'Age'] = 6
+    dataset.loc[ dataset['Age'] > 66, 'Age'] = 6
