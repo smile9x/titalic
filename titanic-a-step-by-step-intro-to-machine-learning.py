dae30aa1aed0d3b9341abfaff378c007b0dc2f94
Cuong Ngo <smile9x@gmail.com>
2018-05-05 16:41:14 +0700

    init intro machine learning file

---
+++ titanic-a-step-by-step-intro-to-machine-learning.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat May  5 16:16:49 2018
+
+@author: Administrator
+"""
+
+# practice follow https://www.kaggle.com/ydalat/titanic-a-step-by-step-intro-to-machine-learning
+
+#Load libraries
+import pandas as pd
+
+import numpy as np
+
+import re
+
+import matplotlib.pyplot as plt
+
+#%matplotlib inline
+import plotly.offline as py
+
+from matplotlib import pyplot
+py.init_notebook_mode(connected=True)
+import plotly.graph_objs as go
+import plotly.tools as tls
+from collections import Counter
+
