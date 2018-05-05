159c41190a94e83129b8afd04fa14304c7e1515a
Cuong Ngo <smile9x@gmail.com>
2018-05-06 00:07:47 +0700

    install packages

--- titanic-a-step-by-step-intro-to-machine-learning.py
+++ titanic-a-step-by-step-intro-to-machine-learning.py
@@ -7,7 +7,7 @@ Created on Sat May  5 16:16:49 2018
 
 # practice follow https://www.kaggle.com/ydalat/titanic-a-step-by-step-intro-to-machine-learning
 
-#Load libraries
+#Load libraries for analysis and visualization
 import pandas as pd
 
 import numpy as np
@@ -25,3 +25,33 @@ import plotly.graph_objs as go
 import plotly.tools as tls
 from collections import Counter
 
+# machine learning libraries
+import xgboost as xgb
+
+import seaborn as sns
+
+import sklearn
+
+from sklearn.linear_model import LogisticRegression
+from sklearn.svm import SVC, LinearSVC
+from sklearn.ensemble import (RandomForestClassifier, AdaBoostClassifier, GradientBoostingClassifier, ExtraTreesClassifier, VotingClassifier)
+
+from sklearn.cross_validation import KFold
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.naive_bayes import GaussianNB
+from sklearn.linear_model import Perceptron
+from sklearn.linear_model import SGDClassifier
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.model_selection import GridSearchCV, cross_val_score, StratifiedKFold, learning_curve
+from sklearn.preprocessing import StandardScaler
+
+import warnings
+warnings.filterwarnings('ignore')
+
+# 1.2 loading dataset
+train = pd.read_csv('C:/Users/Administrator/Desktop/titalic/train.csv')
+test = pd.read_csv('C:/Users/Administrator/Desktop/titalic/test.csv')
+
+PassengerId = test['PassengerId']
+
+train.head(5)
